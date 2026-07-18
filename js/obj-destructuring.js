9c5a5c3198aeb9058f2e171eebc111aed377cac3
MaliKaran786 <145322843+MaliKaran786@users.noreply.github.com>
2026-07-18 13:33:27 +0000

    new addition example

--- js/obj-destructuring.js
+++ js/obj-destructuring.js
@@ -9,3 +9,19 @@ const {age}=obj;
 console.log(age);
 
 // APIs are in JSON format which are objects in itself and remember that in JSON even key are under "" along with the values 
+const user1 = {
+    name: "Karan",
+    age: 20
+};
+
+const user2 = {
+    name: "Rahul",
+    age: 25
+};
+
+function printUser({ name, age }) {
+    console.log(name, age);
+}
+
+printUser(user1);
+printUser(user2);
