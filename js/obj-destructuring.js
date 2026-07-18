ecf0d8bd86b597832421f6b1efd2e3d76ccfe522
MaliKaran786 <145322843+MaliKaran786@users.noreply.github.com>
2026-07-18 11:25:04 +0000

    Add object destructuring example

---
+++ js/obj-destructuring.js
@@ -0,0 +1,11 @@
+let obj={
+    fname:"Karan",
+    lname:"Malik",
+    age  : 21,
+    ht:175,
+}
+
+const {age}=obj;
+console.log(age);
+
+// APIs are in JSON format which are objects in itself and remember that in JSON even key are under "" along with the values 
